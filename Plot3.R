07797cbfe50a5ce50e994e5868cff016e242428b
shiningbamboo <126408026+shiningbamboo@users.noreply.github.com>
2023-03-09 15:59:20 +0530

    Add files via upload

---
+++ Plot3.R
@@ -0,0 +1,21 @@
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+width <- 480
+height <- 480
+png("Plot3.png", width = width, height = height)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
---
+++ Plot4.R
@@ -0,0 +1,23 @@
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+ 
+ globalActivePower <- as.numeric(subsetdata$Global_active_power)
+ globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+ voltage <- as.numeric(subsetdata$Voltage)
+ subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+ subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+ subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+ par(mfrow = c(2, 2)) 
+ # First plot
+ plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+ # Second plot
+ plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+ # Third plot
+ plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+ lines(datetime, subMetering2, type="l", col="red")
+ lines(datetime, subMetering3, type="l", col="blue")
+ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+ # Fourth plot
+ plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power", cex=0.2)
+ 
